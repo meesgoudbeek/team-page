c7c144399722938f4fa7f9919f6c826facbc1e98
Mees Goudbeek <meesgoudbeek@protonmail.com>
2022-11-24 12:07:01 +0100

    added fetch hook for data and displayed data on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import './styles/components/App.scss';
+import useFetch from './services/useFetch';
 
 function App() {
+  const url = 'https://www.mocky.io/v2/5c5adbbf32000052234ec7b2';
+  const { response } = useFetch(url);
+  console.log(response);
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <h1>TEAM</h1>
+      {response && (
+        <div className="grid-row">
+          {response.map((member: any, index: number) => {
+            return (
+              <div key={member.name} className="grid-item">
+                <div className="card">
+                  <div className="card__container">
+                    <div className="image">
+                      <img src={member.image + '?' + index} alt="Team member" />
+                    </div>
+                    <div className="content">
+                      <p>{member.name}</p>
+                      <p>{member.role}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
